e7e88dc2f23973b992a370002e8d76a148489d75
Luis <luisf.infu@gmail.com>
2025-08-12 16:07:00 -0300

    update

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const yf = require('yahoo-finance2').default;
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =  3000; // process.env.PORT 
 
 app.use(cors());
 app.use(express.json());
